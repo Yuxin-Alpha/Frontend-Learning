42c1d23fbde1f516bec9c65bb00ff85e05bd393e
Yuxin <956163372@qq.com>
2019-10-28 23:09:23 +0800

    容器

--- 剑指offer/test.cpp
+++ 剑指offer/test.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include<string>
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
+#include<vector>
+using namespace std;
 
 int main() {
-  string str = "some strings";
-  for (auto c : str) {
-    cout << c << endl; 
+  vector<int> num;
+  for(int i = 0; i < 10; i++) {
+    num.push_back(i);
+    cout << num[i] << ",";
   }
+  cout << "\n";
   return 0;
 }
